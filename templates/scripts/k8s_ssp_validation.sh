8cc52fe4ae5abeb23ce512aab8b5cdd05193f746
Olawale Olaleye <eoalola@amazon.com>
2023-08-16 11:14:17 +0100

    added policies to skip for codebuild

--- templates/scripts/k8s_ssp_validation.sh
+++ templates/scripts/k8s_ssp_validation.sh
@@ -17,8 +17,13 @@ echo "------------------------"
 if (( ${k8sCheckov} == "Y"))
 then
     echo "## VALIDATION : Running checkov ..."
-    #checkov -s -d .
-    checkov -o junitxml --framework kubernetes -d ./kubernetes >checkov.xml
+    # k8s policies https://www.checkov.io/5.Policy%20Index/kubernetes.html
+    # checkov -s -d .
+    # no policy skipped
+    # checkov -o junitxml --framework kubernetes -d ./kubernetes >checkov.xml
+    # skip some checks - https://www.checkov.io/2.Basics/Suppressing%20and%20Skipping%20Policies.html
+    checkov -o junitxml --framework kubernetes -d ./kubernetes --skip-check CKV_K8S_37,CKV_K8S_28,CKV_K8S_43,CKV_K8S_8,CKV_K8S_9,CKV_K8S_40,CKV_K8S_21,CKV_K8S_23,CKV2_K8S_6,CKV_K8S_35 >checkov.xml
+    
 fi
 k8sCheckovOutput=$?
 
@@ -38,4 +43,11 @@ else
   # When validation checks fails, build process is halted.
   echo "## ERROR : Validation Failed"
   exit 1;
+fi
+
+
+if (( $k8sCheckovOutput != 0 ))
+then
+  echo "## ERROR : Checkov Validation Test Failed - Check CodeBuild Test Report"
+  echo "error code: $k8sCheckovOutput"
 fi
